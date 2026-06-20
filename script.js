ee1040299b0fb70d2038d7335876e5ea6fd5bf26
Fer Catana <fernandocatanauwu@gmail.com>
2026-06-20 00:32:13 -0600

    Updated Website

---
+++ script.js
@@ -0,0 +1,10 @@
+// Efecto para el header al hacer scroll
+const header = document.getElementById('main-header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
